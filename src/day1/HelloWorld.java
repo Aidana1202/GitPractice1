89a6559b56b6800835211d2dc56bf67ec1766030
Aidana B <dana12021996@gmail.com>
2021-04-24 16:54:32 -0400

    added more work after publishing to GitHub

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("Adding GitHub now");
         System.out.println("YAY stuff are on github");
+        System.out.println("I pushe to GitHub by myself");
     }
 }
